b6ca4135485698267ba4d686ae1c27ea41cf8a42
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-03 22:12:16 +0200

    Added faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,14 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //
+        for($i = 0; $i < 20; $i++){
+            
+            $project = new Project();
+            $title = $faker->sentence(10);
+            $project -> title = $title;
+            $project -> slug = Str::slug($title);
+            $project -> description = $faker->text(200);
+            $project -> starting_date = $faker->dateTime();
+            $project -> link = $faker->url();
+
+            $project->save();
+        }
     }
 }
